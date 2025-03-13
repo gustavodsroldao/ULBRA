45215bb1c66d9a1a1d3c172db194665afe182206
gustavodsroldao <gustavodsroldao@gmail.com>
2025-03-12 21:42:59 -0300

    exercicios py

--- aula2/exercicio2.py
+++ aula2/exercicio2.py
@@ -2,8 +2,8 @@
 # Crie váriaveis de tipos diferentes e imprima na tela o tipo de cada uma delas.
 # e use type(variavel) para identificar o tipo de cada uma.
 
-caractere = "G"
-inteiro = 10
-real = 10.5
-booleano = True
-print(type(caractere, inteiro, real, booleano))
+caractere: str = "G"
+inteiro: int = 10
+real: float = 10.5
+booleano: bool = True
+print(type(caractere), type(inteiro), type(real), type(booleano))
--- aula2/exercicio3.py
+++ aula2/exercicio3.py
@@ -3,7 +3,4 @@
 # Converta esse número para inteiro e float, imprimindo os dois resultados.
 
 numero = input("Digite um número: ")
-numero = int(numero)
-print(numero)
-numero = float(numero)
-print(numero)
+print (numero, float(numero))
