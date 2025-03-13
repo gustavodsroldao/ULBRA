9b2ea232403c7fbc8e3b7dd5935cad16cd6d7f37
gustavodsroldao <gustavodsroldao@gmail.com>
2025-03-12 21:47:12 -0300

    fix: media

--- logica/aula2/exercicio5.py
+++ logica/aula2/exercicio5.py
@@ -4,4 +4,4 @@
 nota1 = int(input("Digite a primeira nota: "))
 nota2 = int(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
-print("A média é:", media)
+print(f"A média das notas é: {int(media)}")
