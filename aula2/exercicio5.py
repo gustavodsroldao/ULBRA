f6c1dd35e411b895873cf6884e4dc0e60ef722ce
Gustavo Roldão <153242164+gustavodsroldao@users.noreply.github.com>
2025-03-18 19:23:24 -0300

    fix: repositorio ulbra

--- aula2/exercicio5.py
+++
@@ -1,7 +0,0 @@
-# Peça ao usuario duas notas e calcule a média, exiba a média formatada
-# em um numero inteiro
-
-nota1 = int(input("Digite a primeira nota: "))
-nota2 = int(input("Digite a segunda nota: "))
-media = (nota1 + nota2) / 2
-print(f"A média é: {int(media)}")
