fb7ce6a149343c7c12434165ea620c95e03cf695
gustavodsroldao <gustavodsroldao@gmail.com>
2025-03-19 20:23:49 -0300

    feat: exercicios em aula

---
+++ logica/aula3/exercicio2.py
@@ -0,0 +1,11 @@
+# 2. Verificação de Idade
+#Peça ao usuário sua idade e use uma estrutura condicional para informar se ele pode votar e se o voto é obrigatório,
+# se ele não pode votar ou se o voto é opcional.
+
+idade = int(input("Digite sua idade: "))
+if idade < 16:
+    print("Você não pode votar")
+elif idade < 18 or idade >= 70:
+    print("Seu voto é opcional")
+else:
+    print("Seu voto é obrigatório")
